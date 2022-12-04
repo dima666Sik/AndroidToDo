6a7d21c3822510abcce02f10c27626c0ab7b3412
Lazyness <leniviy.demon.1@gmail.com>
2022-12-05 00:59:17 +0200

    Create connection with firebase, update design.

--- app/src/main/java/ua/gura/com/example/androidtodo/MainActivity.java
+++ app/src/main/java/ua/gura/com/example/androidtodo/MainActivity.java
@@ -1,10 +1,12 @@
 package ua.gura.com.example.androidtodo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.imageView).setOnClickListener((view) -> {
-            Intent intent = new Intent(this,
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(()->{
+                        Intent intent = new Intent(this,
                     BaseActivity.class);
             startActivity(intent);
-        });
+        },4000);
     }
 }
